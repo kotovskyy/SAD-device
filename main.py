dc72583a90da610ab8f43ecb12981fd704793ae3
Danylo <danilkoval2004@gmail.com>
2024-06-15 20:07:21 +0200

    Device setup

--- main.py
+++ main.py
@@ -21,11 +21,14 @@ def save_config(config):
 def configure_wifi(ssid, password):
     sta = network.WLAN(network.STA_IF)
     sta.active(True)
+    
+    print(ssid, password)
+    
     sta.connect(ssid, password)
-    for _ in range(10): 
+    for _ in range(30): 
         if sta.isconnected():
             break
-        time.sleep(1)
+        time.sleep(0.5)
     if not sta.isconnected():
         print('Failed to connect to WiFi')
         return False
@@ -54,14 +57,15 @@ async def send_measurement_data(api_url, data, headers):
 def start_ap():
     ap = network.WLAN(network.AP_IF)
     ap.active(True)
-    ap.config(essid='ESP32', password='password')
+    ap.config(essid='ESP32', password='password', authmode=network.AUTH_WPA2_PSK)
+    ap.ifconfig(('192.168.4.1', '255.255.255.0', '192.168.4.1', '8.8.8.8'))
     print('AP IP address:', ap.ifconfig()[0])
     return ap
 
 # Function to fetch settings
-async def fetch_settings(api_url, headers):
+async def fetch_settings(api_url, headers, device_id):
     data = {
-        "device": load_config()['DEVICE_ID']
+        "device": device_id
     }
     query_params = '&'.join([f"{k}={v}" for k, v in data.items()])
     full_url = f"{api_url}?{query_params}"
@@ -103,13 +107,11 @@ def listen_for_config(ap):
             config = load_config()
             config['WIFI_SSID'] = ssid
             config['WIFI_PASS'] = password
-            config['DEVICE_NAME'] = device_id
+            config['DEVICE_NAME'] = device_name
             config['TOKEN'] = token
             config['MAC_ADDRESS'] = mac_address
             save_config(config)
             
-            response = f'{mac_address}'
-            cl.send(response.encode('utf-8'))
             machine.reset() 
         except Exception as e:
             print('Error parsing configuration:', e)
@@ -119,28 +121,44 @@ def listen_for_config(ap):
     ap.active(False)
     s.close()
 
-async def send_measurements_loop(api_url, headers):
+async def create_device(api_url, data, headers):
+    try:
+        response = urequests.post(api_url, json=data, headers=headers)
+        response_data = response.json()
+        print('Response from server:', response.text)
+        
+        config = load_config()
+        config['DEVICE_ID'] = response_data['id']
+        config['CREATED'] = 1
+        
+        save_config(config)
+        
+        response.close()
+    except Exception as e:
+        print('Error sending data:', e)
+
+async def send_measurements_loop(api_url, headers, device_id):
     while True:
         temperature, humidity = measure.measure()
         request_temperature = {
-            "device": load_config()['DEVICE_ID'],
+            "device": device_id,
             "value": temperature,
             "type": 1
         }
         request_humidity = {
-            "device": load_config()['DEVICE_ID'],
+            "device": device_id,
             "value": humidity,
             "type": 1
         }
-        
+        print(device_id)
         await send_measurement_data(api_url + "/measurements/", request_temperature, headers)
         await send_measurement_data(api_url + "/measurements/", request_humidity, headers)
         
         await asyncio.sleep(5) #Send data every 1 minute in the future
 
-async def fetch_settings_loop(api_url, headers):
+async def fetch_settings_loop(api_url, headers, device_id):
     while True:
-        await fetch_settings(api_url + "/settings/", headers)
+        await fetch_settings(api_url + "/settings/", headers, device_id)
         await asyncio.sleep(10) #Fetch every 5 minutes in production
 
 async def main():
@@ -149,13 +167,34 @@ async def main():
         "Authorization": "Token " + config['TOKEN']
     }
     
+    print(config)
+    
     if not configure_wifi(config['WIFI_SSID'], config['WIFI_PASS']):
         ap = start_ap()
         listen_for_config(ap)
     
+    config = load_config()  # Reload the config after potential reset
+
+    if config['CREATED'] == 0:
+        await create_device(config['API_URL'] + "/devices/", {
+            "name": config['DEVICE_NAME'],
+            "mac_address": config['MAC_ADDRESS'],
+            "type": 1
+        }, headers)
+        config = load_config()  # Reload the config after device creation
+        config['CREATED'] = 1
+        save_config(config)
+    else:
+        pass  # Patch device if necessary
+
+    config = load_config()
+    device_id = config['DEVICE_ID']
+    
+    print(config)
+    
     await asyncio.gather(
-        send_measurements_loop(config['API_URL'], headers),
-        fetch_settings_loop(config['API_URL'], headers)
+        send_measurements_loop(config['API_URL'], headers, device_id),
+        fetch_settings_loop(config['API_URL'], headers, device_id)
     )
 
 # Run the main function
